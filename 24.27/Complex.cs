0e8adfbe476addbc5aae7558b1ee673c086136c3
nipol <nipol@LAPTOP-5ALMU75U>
2024-05-14 18:30:33 +0200

    Aggiungere i file di progetto.

---
+++ 24.27/Complex.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._27
+{
+    internal class Complex
+    {
+        private readonly double _A;
+        private readonly double _B;
+        public double A { get { return _A; } }
+        public double B { get { return _B; } }
+        public Complex(double a, double b)
+        {
+            _A = a;
+            _B = b;
+        }
+        public static Complex Parse(string str)
+        {
+            string[] componenti = str.Split(',');
+            return new Complex(double.Parse(componenti[0]), double.Parse(componenti[1]));
+
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} + {1} i", A, B);
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex(c1.A + c2.A, c1.B + c2.B);
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex(c1.A - c2.A, c1.B - c2.B);
+        }
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            return new Complex(c1.A * c2.A - c1.B * c2.B, c1.A * c2.B + c1.B * c2.A);
+        }
+        public static Complex operator *(Complex c1, double x)
+        {
+            return new Complex(c1.A * x, c1.B * x);
+        }
+        public static Complex operator /(Complex c1, double x)
+        {
+            return new Complex(c1.A / x, c1.B / x);
+        }
+        public static bool operator ==(Complex c1, Complex c2)
+        {
+            if (object.ReferenceEquals(c1, null) && object.ReferenceEquals(c2, null))
+            {
+                return true;
+            }
+            if(object.ReferenceEquals(c1,null) || object.ReferenceEquals(c2, null))
+            {
+                return false;
+            }
+            return (c1.A == c2.A && c1.B == c2.B);
+        }
+        public static bool operator !=(Complex c1, Complex c2)
+        {
+            return !(c1 == c2);
+        }
+        public double Modulo()
+        {
+            return Math.Sqrt(A * A + B * B);
+        }
+        
+        public double Angolo()
+        {
+            return (Math.Atan(B / A)) * 180 / Math.PI;
+        }
+    }
+}
---
+++ 24.27/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("numero complesso 1:");
+            Complex c1 = Complex.Parse(Console.ReadLine());
+            Console.WriteLine("numero complesso 2:");
+            Complex c2=Complex.Parse(Console.ReadLine());
+            Console.WriteLine("numero scalare:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("somma: {0}", c1 + c2);
+            Console.WriteLine("differenza: {0}", c1 - c2);
+            Console.WriteLine("prodotto 1: {0}", c1 * c2);
+            Console.WriteLine("prodotto 2: {0}", c1 * x);
+            Console.WriteLine("divisione: {0}", c1 / x);
+            if (c1 == c2)
+            {
+                Console.WriteLine("uguali");
+            }
+            else
+            {
+                Console.WriteLine("diversi");
+            }
+            Complex c = new Complex(c1.A, c1.B);
+            Complex d = new Complex(c2.A, c2.B);
+            Console.WriteLine("modulo c1: {0}", c1.Modulo());
+            Console.WriteLine("modulo c2: {0}", c2.Modulo());
+            Console.WriteLine("angolo c1: {0}", c1.Angolo());
+            Console.WriteLine("angolo c2: {0}", c2.Angolo());
+            Console.ReadLine();
+        }
+    }
+}
